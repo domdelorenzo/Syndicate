bf57d602b9a6ce89ef48c926ba4a5d764cce3a34
domdelorenzo <delorenzo.breed@gmail.com>
2022-02-08 16:05:50 -0600

    Moved key to .env

--- syndicate_django/settings.py
+++ syndicate_django/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--f$edaj!-jupy0mlj!4dbxp0=2+_ly40n4f*le#ck1^cnxv=0s'
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
