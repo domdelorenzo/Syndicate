8ac62011abeb968d9820bcdeefdba4f3033aed5c
domdelorenzo <delorenzo.breed@gmail.com>
2022-02-08 16:03:40 -0600

    Initialized Auth0 login

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+from . import views
+
+# urlpatterns = [
+#   path('accounts/login/', auth_views.LoginView.as_view(template_name='acounts/login.html'), name='login'),
+#   path('acccounts/logout', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+# ]
+
+
+urlpatterns = [
+    path('', views.index),
+    path('dashboard', views.dashboard),
+    path('logout', views.logout),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('social_django.urls')),
+]
