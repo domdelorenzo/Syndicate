19090d4a66fe6eca65a5717073bc2cd720a41361
domdelorenzo <delorenzo.breed@gmail.com>
2022-02-13 20:06:50 -0600

    Added auth controller and routes. Tested user registration via insomnia

--- client/src/Services/endpoints.js
+++ client/src/Services/endpoints.js
@@ -69,3 +69,34 @@ export const DeleteFeed = async (id) => {
     throw error;
   }
 };
+
+/* User Auth */
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/register', data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const LogInUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/login', data);
+    localStorage.setItem('token', res.data.token);
+    localStorage.setItem('id', res.data.user.id);
+    return res.data.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const CheckSession = async () => {
+  try {
+    const res = await Client.get('/auth/session');
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
